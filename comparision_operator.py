de1b851ac35ab89a5281c99a5e30435a350f7390
gottipalliganesh36-source <gottipalliganesh36@gmail.com>
2026-08-31 15:56:35 +0530

    arithmetic operators

---
+++ comparision_operator.py
@@ -0,0 +1,13 @@
+#python cpmparision operators exercise
+a = 10
+b = 5
+result = a > b #greater than comparision operator
+print("result of",a,">",b,"is:",result)
+
+
+result= a<b#less than comparion operator 
+print("result of",a,"<",b,"is:",result)
+
+
+result = a==b # equal to comparision operator
+print("result of",a,"==",b,"is:",result)
