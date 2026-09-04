9ba53e9898ebdc93b48d5d7f0018fb8e3a0f17d6
gottipalliganesh36-source <gottipalliganesh36@gmail.com>
2026-09-04 14:24:11 +0530

    membership operators

---
+++ membership_operators.py
@@ -0,0 +1,26 @@
+# Membership Opertors
+fruits = ["apple", "banana", "mango"]
+#by using in operator
+print("apple" in fruits)
+print("grapes" in fruits)
+print("mango" in fruits)
+#by using  not in operator
+fruits = ["apple", "banana", "mango"]
+print("grapes"  not in fruits)
+print("mango"  not in fruits)
+
+
+
+#membership operator 
+result= "a" in "apple"
+print("Result of a in apple is:",result)
+result= "a" not in "apple"
+print("Result of a is not in apple is:",result)
+
+
+# another example of membership operators
+a= [4,3,2]
+print("2 in a is:",2 in a)
+print("2 is not in a is:",2 not in a) 
+print("5 in a is:",5 in a)
+print("5 is not in a is:",5 not in a)
